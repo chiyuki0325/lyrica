e3dde564bc7fd2e5fb290b3b9fde43936efe5d4b
Kirikaze Chiyuki <me@chyk.ink>
2024-07-03 21:46:08 +0800

    [fix] netease song name unwrap

--- src/lyric_providers/netease.rs
+++ src/lyric_providers/netease.rs
@@ -6,7 +6,7 @@ use serde_json::{
 use std::time::Duration;
 use crate::lyric_parser::{
     LyricLine,
-    parse_netease_lyrics
+    parse_netease_lyrics,
 };
 
 #[derive(Clone)]
@@ -26,21 +26,23 @@ impl NeteaseLyricProvider {
             // 搜索有结果
             let search_result: Value = from_json_str(&search_result).unwrap();
             for song in search_result["result"]["songs"].as_array().unwrap_or(&Vec::new()) {
-                if song["name"].as_str().unwrap().to_ascii_lowercase().starts_with(
-                    metadata.title().unwrap_or_default().to_ascii_lowercase().as_str()
-                ) {
-                    let searched_length = Duration::from_millis(song["duration"].as_u64().unwrap());
-                    let music_length = metadata.length().unwrap_or_default();
-                    if music_length.checked_sub(searched_length).unwrap_or_default() < Duration::from_secs(6) {
-                        // 相差不超过 6 秒
-                        let lyric_result = ncm_api.song_lyric(song["id"].as_u64().unwrap()).await;
-                        if let Ok(lyric_result) = lyric_result {
-                            let lyric_lines = lyric_result.lyric;
-                            let tlyric_lines = lyric_result.tlyric;
-                            return (
-                                parse_netease_lyrics(lyric_lines, tlyric_lines),
-                                true
-                            )
+                if let Some(name) = song.get("name") {
+                    if name.as_str().unwrap_or_default().to_ascii_lowercase().starts_with(
+                        metadata.title().unwrap_or_default().to_ascii_lowercase().as_str()
+                    ) {
+                        let searched_length = Duration::from_millis(song["duration"].as_u64().unwrap());
+                        let music_length = metadata.length().unwrap_or_default();
+                        if music_length.checked_sub(searched_length).unwrap_or_default() < Duration::from_secs(6) {
+                            // 相差不超过 6 秒
+                            let lyric_result = ncm_api.song_lyric(song["id"].as_u64().unwrap()).await;
+                            if let Ok(lyric_result) = lyric_result {
+                                let lyric_lines = lyric_result.lyric;
+                                let tlyric_lines = lyric_result.tlyric;
+                                return (
+                                    parse_netease_lyrics(lyric_lines, tlyric_lines),
+                                    true
+                                );
+                            }
                         }
                     }
                 }
