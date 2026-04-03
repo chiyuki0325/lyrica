c1c6e254f61843824f741a343a3f3b784d5eb827
Kirikaze Chiyuki <me@chyk.ink>
2026-04-03 21:24:07 +0800

    [chore] update build script

--- frontend/build_kde_plasmoid.sh
+++ frontend/build_kde_plasmoid.sh
@@ -6,14 +6,20 @@ else
   arch="$1"
 fi
 
-version=$(jq -cr ".KPlugin.Version" plasmoid/metadata.json)
+root_dir=$(dirname "$(cargo locate-project --workspace --message-format plain)")
+pushd root_dir > /dev/null
+
+version=$(grep -E '^version\s*=\s*".*"' Cargo.toml | sed -E 's/version\s*=\s*"(.*)"/\1/')
 distro=$(kreadconfig6 --file /etc/os-release --group "<default>" --key "ID")
 
 cargo build --release --target "${arch}-unknown-linux-gnu"
 
 mkdir package
 cp -r plasmoid/* package/
+sed -i "s/LYRICA_VERSION/${version}/g" package/contents/metadata.json
 mkdir -p package/contents/bin/
 cp "target/${arch}-unknown-linux-gnu/release/lyrica" package/contents/bin/
 7z a -tzip "lyrica-plasmoid-v${version}-${distro}-${arch}.plasmoid" package/*
 rm -rf package
+
+popd > /dev/null
