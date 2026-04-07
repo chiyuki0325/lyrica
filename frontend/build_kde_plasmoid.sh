abc7cf2b176c7e38b652594bdcb6a3034ccef3d9
Kirikaze Chiyuki <me@chyk.ink>
2026-04-07 22:41:15 +0800

    [fix:front:kde] fix build script

--- frontend/build_kde_plasmoid.sh
+++ frontend/build_kde_plasmoid.sh
@@ -6,8 +6,8 @@ else
   arch="$1"
 fi
 
-root_dir=$(dirname "$(cargo locate-project --workspace --message-format plain)")
-pushd root_dir > /dev/null
+root_dir="$(dirname "$(cargo locate-project --workspace --message-format plain)")"
+pushd "$root_dir" > /dev/null
 
 version=$(grep -E '^version\s*=\s*".*"' Cargo.toml | sed -E 's/version\s*=\s*"(.*)"/\1/')
 distro=$(kreadconfig6 --file /etc/os-release --group "<default>" --key "ID")
@@ -15,7 +15,7 @@ distro=$(kreadconfig6 --file /etc/os-release --group "<default>" --key "ID")
 cargo build --release --target "${arch}-unknown-linux-gnu"
 
 mkdir package
-cp -r plasmoid/* package/
+cp -r frontend/kde/* package/
 sed -i "s/LYRICA_VERSION/${version}/g" package/contents/metadata.json
 mkdir -p package/contents/bin/
 cp "target/${arch}-unknown-linux-gnu/release/lyrica" package/contents/bin/
